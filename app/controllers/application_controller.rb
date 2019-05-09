b7a9bb63c4445ffd88052937210a258ba0e129f7
mona <ms@gmail.com>
2019-05-09 20:59:15 +0530

    login done with association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
+	before_action :store_user_location!, if: :storable_location?
 	protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	private
+	
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 
 	protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+  before_validation :set_default_role
   belongs_to :role
+  validates :name, presence: true
+  protected
+  def set_default_role
+  	binding.pry
+  	role=Role.find_by_name("customer")
+  	binding.pry
+    self.role_id = role.id
+  end
 end
